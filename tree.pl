4c0c20049dcf6174a8324f0004be79d17e198223
wentingjiang2022 <85434354+wentingjiang2022@users.noreply.github.com>
2022-01-11 09:15:38 +0100

    Update tree.pl

--- tree.pl
+++ tree.pl
@@ -1,9 +1,15 @@
-preorder(nil, []).
-preorder(L - M + R, L3):-
-    preorder(L,L1),
-    preorder(R,R1),
-    append(L1,M,L2),
-    append(L2,R1,L3).
+% part one
+preorder((Left-SomeList+Right), ValueList):-
+    preorder(Left,LeftList),
+    preorder(Right,RightList),
+    append([SomeList],LeftList,Temp),
+    append(Temp,RightList,ValueList).
+preorder(nil,[]).
+
+% preorder((nil-[f(a,b)]+(nil-[21,22,23]+nil))-[root,1,2]+((nil-[a]+nil)-[child,4]+nil),L).
+
+
+% part two
 
 %With difference lists NOT DONE BECAUSE I DO NOT UNDERSTAND THEM
 preorder((Left-SomeList+Right), ValueList):-
