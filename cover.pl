079475e663e1fe610cf9cc306d224512251f2bf6
wentingjiang2022 <85434354+wentingjiang2022@users.noreply.github.com>
2022-01-11 08:27:20 +0100

    Update cover.pl

--- cover.pl
+++ cover.pl
@@ -1,1 +1,49 @@
 % exact cover problem
+% someone's answer
+
+
+%exercise 5 [[c,e],[a,d,g],[b,c,f],[a,d,f],[b,g],[d,e,g]]
+isCovered(I,[H|_],O):-
+    m(I,H),
+    O = H.
+isCovered(I,[_|T],O):-
+    isCovered(I,T,O).
+
+residual(Is,Os,O,NIs,NOs):-
+    removeItems(Is,O,NIs),
+    removeOptions(Os,O,NOs).
+
+removeOption([H|T],I,R):-
+    m(I,H),
+    removeOption(T,I,R).
+removeOption([H|T],I,R):-
+    not(m(I,H)),
+    removeOption(T,I,Temp),
+    R = [H|Temp].
+removeOption([],_,[]).
+
+removeOptions(Os,[H|T],R):-
+    removeOption(Os,H,Temp),
+    removeOptions(Temp,T,R).
+removeOptions(Os,[],Os).
+
+removeItems(Is,[],Is).
+removeItems(Is,[H|T],R):-
+    remove(H,Is,Temp),
+    removeItems(Temp,T,R).
+
+remove(I,[I|T],T).
+remove(I,[H|T],R):-
+    I \= H,
+    remove(I,T,Temp),
+    R = [H|Temp].
+
+m(H,[H|_]).
+m(E,[_|T]):-
+    member(E,T).
+
+findexactcovering([H|T],Os,Acc,Result):-
+    isCovered(H,Os,O),
+    residual([H|T],Os,O,NIs,NOs),
+    findexactcovering(NIs,NOs,[O|Acc],Result).
+findexactcovering([],_,Acc,Acc).
