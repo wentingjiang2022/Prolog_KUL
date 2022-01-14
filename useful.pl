92a7efcaa9f4eef86b049ee4eb40a5561cea1d99
wentingjiang2022 <85434354+wentingjiang2022@users.noreply.github.com>
2022-01-14 18:28:32 +0100

    Update useful.pl

--- useful.pl
+++ useful.pl
@@ -208,31 +208,6 @@ try_action(CurrentState,NewState):-
 validate_state(NewState):-
 
 
-%%%%
-
-% find subset of list
-% implemented by someone last year
-/* Without control on order */
-sublist1([],_).
-sublist1([H|T],List):-
-    member1(H, List),
-    sublist1(T, List).
-
-member1(_, []) :- fail.
-member1(X, [H|T]) :-
-    H = X;
-    H \= X,
-    member1(X, T).
-
-/* With control on order */
-sublist2(S,L) :- 
-    append1(_,L2,L),
-    append1(S,_,L2).
-
-append1([],X,X).
-append1([H|T1], X, [H|T2]  ) :- append1(T1,X,T2).
-
-
 % operations
 
 1). =:=.   arithmetically identical
